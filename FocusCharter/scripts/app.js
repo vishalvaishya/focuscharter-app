28044ed77fad214ce0ee93fa44cd38070faee7b8
Vishal <vishal.vaishya12@gmail.com>
2015-11-19 06:53:52 -0600

    Trial

--- FocusCharter/scripts/app.js
+++ FocusCharter/scripts/app.js
@@ -1,49 +1,74 @@
+// store a reference to the application object that will be created
+// later on so that we can use it if need be
+var app;
 
-(function () {
-
-    // store a reference to the application object that will be created
-    // later on so that we can use it if need be
-    var app;
-
-    // create an object to store the models for each view
-    window.APP = {
-      models: {
+// create an object to store the models for each view
+window.APP = {
+    models: {
         home: {
-          title: 'Home'
+            title: 'Home'
         },
         settings: {
-          title: 'Settings'
+            title: 'Settings'
+        },
+        childrens: {
+            title: 'Childrens'
+            //ds: new kendo.data.DataSource({
+            //    schema: {
+            //        model: {
+            //            id: "id",
+            //            fields: {
+            //                id: { type: "number" },
+            //                name: { type: "string" },
+            //                schoolid: { type: "string" },
+            //                schoolname: { type: "string" },
+            //                routeno: { type: "string" },
+            //                pickuplocation: { type: "string" },
+            //                pickuploclat: { type: "float" },
+            //                pickuploclng: { type: "float" },
+            //                pickuptime: { type: "string" },
+            //                pickupdays: { type: "string" }
+            //            }
+            //        }
+            //    },
+            //    transport: {
+            //        read: {
+            //            url: "data/bus-live-status.json"
+            //        },
+            //        dataType: "json"
+            //    }
+            //}),
         },
         contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+            title: 'Contacts',
+            ds: new kendo.data.DataSource({
+                data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
+            }),
+            alert: function (e) {
+                alert(e.data.name);
+            }
         }
-      }
-    };
+    }
+};
 
-    // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+// this function is called by Cordova when the application is loaded by the device
+document.addEventListener('deviceready', function () {
+    // hide the splash screen as soon as the app is ready. otherwise
+    // Cordova will wait 5 very long seconds to do it for you.
+    navigator.splashscreen.hide();
 
-      app = new kendo.mobile.Application(document.body, {
-        
+    app = new kendo.mobile.Application(document.body, {
         // comment out the following line to get a UI which matches the look
         // and feel of the operating system
-        skin: 'flat',
-
+        skin: 'nova',        
         // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        initial: 'views/children-list.html'
+    });
 
-    }, false);
-
-
-}());
+    //var telephoneNumber = cordova.require("cordova/plugin/telephonenumber");
+    //    telephoneNumber.get(function (result) {
+    //        console.log("result = " + result);
+    //    }, function (error) {
+    //        console.log("error = " + error.code);
+    //    });
+}, false);
